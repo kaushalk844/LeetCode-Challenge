06cce45066f253eccc8d01a62e558a885082266d
Kaushal Rajput <kaushalk844@gmail.com>
2021-01-09 19:36:09 +0530

    Update Day4-Merge Two Sorted Lists.cpp

--- January Challenges/Day4-Merge Two Sorted Lists.cpp
+++ January Challenges/Day4-Merge Two Sorted Lists.cpp
@@ -1,33 +1,33 @@
 /**
  * Definition for singly-linked list.
- * public class ListNode {
+ * struct ListNode {
  *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2)
-    {
-        ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
-        while (l1 != null && l2 != null)
-        {
-             if(l1.val <= l2.val)
-            {
-                curr.next = l1;
-                l1 = l1.next;
-            }
-            else
-            {
-                curr.next = l2;
-                l2 = l2.next;
-            }
-            curr = curr.next;
+public:
+    
+    ListNode* merge(ListNode* l1, ListNode* l2){
+        if(l1 == NULL) return l2;
+        if(l2 == NULL) return l1;
+        
+        ListNode* h;
+        if(l1->val < l2->val){
+            h = l1;
+            h->next = merge(l1->next,l2);
+        }else{
+            h = l2;
+            h->next = merge(l1,l2->next);
         }
-        curr.next= l1 != null ? l1 : l2;
-        return dummy.next;
+        return h;
     }
-}
+    
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* h = merge(l1,l2);
+        return h;
+    }
+};
