c4db5f9b91bc484aeab30c72e4665b592057caf0
Kaushal Rajput <kaushalk844@gmail.com>
2021-01-16 20:39:37 +0530

    Create Day16-Kth Largest Element in an Array.cpp

---
+++ January Challenges/Day16-Kth Largest Element in an Array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        
+        // Max Heap Solution
+       
+        priority_queue<int> maxHeap;
+        for(auto &i : nums){
+            maxHeap.push(i);
+        }
+        for(int i = 1; i < k; i++){
+            maxHeap.pop();
+        }
+        return maxHeap.top();
+        
+//         // Min Heap Solution
+        
+//         for(int num:nums)
+//         {
+//             minHeap.push(num);
+//             if(minHeap.size() > k)
+//                 minHeappop();
+//         }
+//         return minHeap.top();
+    }
+};
