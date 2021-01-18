79235652122063a83dc12541beafea42d9add117
Kaushal Rajput <kaushalk844@gmail.com>
2021-01-18 20:18:42 +0530

    hash table

--- January Challenges/Day18-Max Number of K-Sum Pairs.cpp
+++ January Challenges/Day18-Max Number of K-Sum Pairs.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        unordered_map<int, int> mp;
-        int cnt = 0;
-        for(int i =0; i < nums.size(); i++){
-            if(mp[k-nums[i]]){
-                mp[k-nums[i]]--;
-                cnt++;
-            }else
-                mp[nums[i]]++;
-            }
-            return cnt;
+        
+        unordered_map<int,int> m;
+        int ans = 0;
+        
+        for(auto x:nums){
+            m[x]++;
         }
+        
+        for(auto it = m.begin() ; it != m.end() ; it++){
+            int num = it->first;
+            int count = it->second;
+            
+            if(k - num == num) ans += count/2;
+            else if(m.count(k-num)){
+                int v = min(m[num], m[k-num]);
+                ans += v;
+                m[num] -= v;
+                m[k-num] -= v;
+            }
+        }
+        
+        return ans;
+    }
 };
