5eca5bb5cb7b32206e5b31d7e17c94b5e81901fb
Kaushal Rajput <kaushalk844@gmail.com>
2021-01-18 20:20:14 +0530

    heap | divide and conquer

--- January Challenges/Day16-Kth Largest Element in an Array.cpp
+++ January Challenges/Day16-Kth Largest Element in an Array.cpp
@@ -24,3 +24,19 @@ public:
 //         return minHeap.top();
     }
 };
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//     class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+        
+//         if(k==0)return -1;
+//         sort(nums.begin(),nums.end());
+//         int n = nums.size();
+        
+//         int v = n-k;
+//         return nums[v];
+        
+        
+        
+//     }
+// };
