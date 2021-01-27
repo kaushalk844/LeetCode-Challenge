7c5152be7f7e85edfa5252d91cbe6849a003d0b2
Kaushal Rajput <kaushalk844@gmail.com>
2021-01-27 20:47:02 +0530

    binary search

---
+++ January Challenges/Day27-Concatenation of Consecutive Binary Numbers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    int mod=1000000007;
+public:
+    int concatenatedBinary(int n) {
+        int res=0;
+        int count=1;
+        for(int i=n;i>=1;i--)
+        {
+             int N=i;
+             while(N>0)
+             {
+                if(N%2==1)
+                res=(res%mod+count%mod)%mod;
+                count=(count*2)%mod;
+                N/=2;
+             }
+        }
+        return res;
+    }
+};
